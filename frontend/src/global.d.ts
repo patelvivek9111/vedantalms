0064c90a6182dcf66c1bdf4b580863711e8c1532
vp4186522 <vp4186522@gmail.com>
2025-11-04 15:17:15 -0500

    Add missing Calendar props to type declarations (popup, selectable, etc)

--- frontend/src/global.d.ts
+++ frontend/src/global.d.ts
@@ -23,6 +23,13 @@ declare module 'react-big-calendar' {
     eventPropGetter?: (event: Event) => any;
     dayPropGetter?: (date: Date) => any;
     slotPropGetter?: (date: Date) => any;
+    popup?: boolean;
+    selectable?: boolean;
+    onNavigate?: (date: Date, view: string, action: NavigateAction) => void;
+    onView?: (view: string) => void;
+    view?: string;
+    date?: Date;
+    [key: string]: any; // Allow any additional props
   }
   export class Calendar extends React.Component<CalendarProps> {}
   export function dateFnsLocalizer(config: any): any;
