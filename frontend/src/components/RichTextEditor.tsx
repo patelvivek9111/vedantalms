b6cf8c4e8941db2df9ae2756426b309de5448cdf
vp4186522 <vp4186522@gmail.com>
2025-11-06 20:59:36 -0500

    Update TinyMCE API key to new key

--- frontend/src/components/RichTextEditor.tsx
+++ frontend/src/components/RichTextEditor.tsx
@@ -1,6 +1,5 @@
-import React, { useMemo } from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import React from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 
 interface RichTextEditorProps {
   content: string;
@@ -11,41 +10,28 @@ interface RichTextEditorProps {
 }
 
 const RichTextEditor: React.FC<RichTextEditorProps> = ({ content, onChange, placeholder, className, height }) => {
-  const modules = useMemo(() => ({
-    toolbar: [
-      [{ 'header': [1, 2, 3, false] }],
-      ['bold', 'italic', 'underline', 'strike'],
-      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-      [{ 'indent': '-1'}, { 'indent': '+1' }],
-      [{ 'align': [] }],
-      ['link', 'image'],
-      ['clean']
-    ],
-    clipboard: {
-      matchVisual: false
-    }
-  }), []);
-
-  const formats = [
-    'header',
-    'bold', 'italic', 'underline', 'strike',
-    'list', 'bullet', 'indent',
-    'align',
-    'link', 'image'
-  ];
-
   return (
-    <div className={className}>
-      <ReactQuill
-        theme="snow"
-        value={content}
-        onChange={onChange}
-        modules={modules}
-        formats={formats}
-        placeholder={placeholder || 'Write something...'}
-        style={{ height: height ? `${height}px` : '200px' }}
-      />
-    </div>
+    <Editor
+      apiKey="gdng5aigkhrb5lsxhh4j8u2s4elts687j9k2uzu63l6zd4gw"
+      value={content}
+      onEditorChange={onChange}
+      init={{
+        height: height || 200,
+        menubar: false,
+        plugins: [
+          'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview', 'anchor',
+          'searchreplace', 'visualblocks', 'code', 'fullscreen',
+          'insertdatetime', 'media', 'table', 'help', 'wordcount'
+        ],
+        toolbar: 'undo redo | formatselect | bold italic underline backcolor | \
+          alignleft aligncenter alignright alignjustify | \
+          bullist numlist outdent indent | removeformat | help',
+        placeholder: placeholder || 'Write something...',
+        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+      }}
+      textareaName="content"
+      className={className}
+    />
   );
 };
 
